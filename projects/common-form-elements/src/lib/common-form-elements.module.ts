439cbf008fbfcd4e96cf7dafb10922228f8e78c8
diptesh6501 <diptesh1993@gmail.com>
2020-06-11 19:42:20 +0530

    Initial commit for select box

--- projects/common-form-elements/src/lib/common-form-elements.module.ts
+++ projects/common-form-elements/src/lib/common-form-elements.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropdownsComponent } from './dropdowns/dropdowns.component';
+import { FormComponent } from './form/form.component';
 import { CommonFormElementsComponent } from './common-form-elements.component';
 
+
 @NgModule({
-  declarations: [CommonFormElementsComponent],
-  imports: [
-  ],
-  exports: [CommonFormElementsComponent]
+  declarations: [CommonFormElementsComponent, DropdownsComponent, FormComponent],
+  imports: [CommonModule],
+  exports: [CommonFormElementsComponent , DropdownsComponent, FormComponent]
 })
 export class CommonFormElementsModule { }
