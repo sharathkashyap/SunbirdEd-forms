fdc28fa8ee4cf6cdc5b80cddc3cd15fe49e00c3a
sharath <sharaths.kashyap@gmail.com>
2020-06-12 11:13:37 +0530

    Issue #0000: Initial Form Commit

---
+++ projects/common-form-elements/src/lib/checkbox/checkbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sb-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.css']
+})
+export class CheckboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
