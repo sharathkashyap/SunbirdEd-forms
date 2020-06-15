945c3610acb87f2e0f2859d08ce814eccad0be01
diptesh6501 <diptesh1993@gmail.com>
2020-06-15 17:28:23 +0530

    added option group component

--- projects/common-form-elements/src/lib/common-form-elements.module.ts
+++ projects/common-form-elements/src/lib/common-form-elements.module.ts
@@ -6,12 +6,14 @@ import { TextboxComponent } from './textbox/textbox.component';
 import { TextareaComponent } from './textarea/textarea.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { FormComponent } from './form/form.component';
+import { OptionGroupComponent } from './option-group/option-group.component';
 
 @NgModule({
-  declarations: [CommonFormElementsComponent, DropdownComponent, TextboxComponent, TextareaComponent, CheckboxComponent, FormComponent],
+  declarations: [CommonFormElementsComponent, DropdownComponent, TextboxComponent,
+     TextareaComponent, CheckboxComponent, FormComponent, OptionGroupComponent],
   imports: [ CommonModule
   ],
   exports: [ CommonFormElementsComponent , DropdownComponent,
-            TextboxComponent, TextareaComponent, CheckboxComponent, FormComponent]
+            TextboxComponent, TextareaComponent, CheckboxComponent, FormComponent, OptionGroupComponent]
 })
 export class CommonFormElementsModule { }
