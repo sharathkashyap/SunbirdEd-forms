1668281d31bf62ebc150479ea3efdf1ee453626a
devarfath <arafath.ahmed@goodworklabs.com>
2020-06-24 17:49:10 +0530

    REMOVED: hardcoded test message, ADDED: default empty options array

--- projects/common-form-elements/src/lib/dropdown/dropdown.component.ts
+++ projects/common-form-elements/src/lib/dropdown/dropdown.component.ts
@@ -29,6 +29,10 @@ export class DropdownComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (!this.options) {
+      this.options = [];
+    }
+
     if (this.isOptionsClosure(changes['options'].currentValue)) {
       this.options$ = changes['options'].currentValue(changes['context'].currentValue).pipe(
         catchError((e) => {
