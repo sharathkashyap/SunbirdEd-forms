0d32ccee2c5cc6cfe29b50b346bf606454573d36
devarfath <arafath.ahmed@goodworklabs.com>
2020-06-26 17:57:03 +0530

    CHANGED: dropdowns to use partialValueComparator

--- projects/common-form-elements/src/lib/utilities/value-comparator.ts
+++ projects/common-form-elements/src/lib/utilities/value-comparator.ts
@@ -1,9 +1,9 @@
 export class ValueComparator {
-  static valueComparator(v1, v2): boolean {
+  static partialValueComparator(v1, v2): boolean {
     if (v1 === v2) {
       return true;
     } else if (typeof v1 === 'object' && typeof v2 === 'object') {
-      return Object.keys(v1).some((k) => ValueComparator.valueComparator(v1[k], v2[k]));
+      return Object.keys(v1).some((k) => ValueComparator.partialValueComparator(v1[k], v2[k]));
     } else if (!v1 && !v2) {
       return true;
     }
